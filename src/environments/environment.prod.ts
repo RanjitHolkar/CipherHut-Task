643bafc061fa9974fa9d2d015c287b31f2b9ee84
ranjitholkar <ranjitholkar75@gmail.com>
2020-11-20 08:18:06 +0530

    Environment set

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl:'http://dummy.restapiexample.com/api/v1/',
+  email:'ranjitholkar81@gmail.com',
+  password: 123456,
+  encryptionKey:'0123456789123456'
 };
